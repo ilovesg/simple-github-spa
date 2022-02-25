96cce985b508b7d78598d8a8b9e9be52bf18ac19
ilovesg <immortalbeing13@gmail.com>
2022-02-25 23:27:12 +0500

    Add repos output

--- src/scripts/components/main/Main.jsx
+++ src/scripts/components/main/Main.jsx
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import getRepos from '../../actions/repos';
+import Repo from '../repo/Repo';
 import './main.scss';
 
 export default function Main() {
+  const dispatch = useDispatch();
+  const repos = useSelector((state) => state.repos.items);
+
+  useEffect(() => {
+    dispatch(getRepos());
+  }, [dispatch]);
+
   return (
     <div className="container">
-      Main component
+      {repos.map((repo) => <Repo repo={repo} key={repo.id} />)}
     </div>
   );
 }
