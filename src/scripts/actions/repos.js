62e5efc8bfc1ad224e88a8a7d95f924d3bdbe293
ilovesg <immortalbeing13@gmail.com>
2022-04-11 18:52:47 +0500

    Set isFetching to true before fetching

--- src/scripts/actions/repos.js
+++ src/scripts/actions/repos.js
@@ -1,8 +1,10 @@
 import axios from 'axios';
-import { setRepos } from '../store/reposReducer';
+import { setRepos, setIsFetching } from '../store/reposReducer';
 
 function getRepos(searchQuery = 'stars:%3E1') {
   return async (dispatch) => {
+    dispatch(setIsFetching(true));
+
     const response = await axios.get(`https://api.github.com/search/repositories?q=${searchQuery}&sort=stars`);
 
     dispatch(setRepos(response.data.items));
