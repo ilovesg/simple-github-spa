a51c4a8e1d40a8751ed6ee0d05744016e71763c4
ilovesg <immortalbeing13@gmail.com>
2022-04-20 00:13:05 +0500

    Render repo data

--- src/scripts/components/card/card.jsx
+++ src/scripts/components/card/card.jsx
@@ -1,13 +1,39 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import { useNavigate, useParams } from 'react-router-dom';
+import { getRepo } from '../../actions/repos';
+import './card.scss';
 
 export default function Card() {
   const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const isFetching = useSelector((state) => state.repos.isFetching);
+  const repo = useSelector((state) => state.repos.repo);
   const { username, repoName } = useParams();
 
+  useEffect(() => {
+    dispatch(getRepo(username, repoName));
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [dispatch]);
+
   return (
-    <div className="card">
-      Card is here.
+    <div className="container">
+      {
+        isFetching
+          ? <div className="preloader" />
+          : (
+            <div className="card">
+              <h1>{repo.name}</h1>
+              <div className="card__img">
+                <img alt="" src={repo.owner.avatar_url} />
+              </div>
+              <div className="card__stars">
+                <strong>Stars:</strong>
+                {repo.stargazers_count}
+              </div>
+            </div>
+          )
+      }
       <button type="button" onClick={() => navigate(-1)}>Go back</button>
     </div>
   );
